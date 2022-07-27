42b442db7e70c883f03be140dc38d592b5b3d6b4
domregius <domregius@gmail.com>
2022-07-27 00:34:17 +0000

    fim aula 3

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask("hello")
 
+posts = [
+    { "title": "meu primeiro post",
+       "body": "aqui é o texto do post",
+       "author": "regis",
+        "created": datetime.datetime(2022,7,25)
+        },
+
+    { "title": "meu segundo post",
+       "body": "aqui é o texto do segundo post",
+       "author": "alberto",
+        "created": datetime.datetime(2022,7,29)
+        }
+]
+
 @app.route("/")
 
-def hello():
-    return "Hello world...."
+def index():
+    return render_template('index.html', posts=posts)
     
-@app.route("/meucontato")
-def zebra():
-    return render_template('index.html')
-        
+@app.route("/login")
+def login():
+    return render_template('login.html', posts=posts)
